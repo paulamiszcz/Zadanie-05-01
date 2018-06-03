af03dc923cffba11de56d6f8adebc6ef08ce78b8
Paula <paulamiszcz@wp.pl>
2018-06-03 21:45:49 +0200

    Stworzenie tablicy z 3 imion i wyświetlenie ich na ekranie

---
+++ src/Tablica.java
@@ -0,0 +1,9 @@
+public class Tablica {
+    public static void main(String[] args) {
+        String[] name = new String [3];
+        name[0] = "Andrzej";
+        name[1] = "Marta";
+        name[2] = "Łukasz";
+        System.out.println("Pierwsze imię: " + name[0] + ", drugie imie: " + name[1] + ", trzecie imie: " + name[2]);
+    }
+}
